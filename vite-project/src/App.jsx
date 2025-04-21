6404e39895680f1a1fde1503fb5f2239777c7db2
Nuttividad-BCS <francisnatividad333@gmail.com>
2025-04-21 16:53:13 +0800

    Changes to be committed: modified:   vite-project/src/App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,49 @@
 import { Canvas } from "@react-three/fiber"
-import { Environment, OrbitControls, useGLTF } from "@react-three/drei"
+import { Environment, OrbitControls, PerspectiveCamera, useGLTF } from "@react-three/drei"
 import { DasMap } from "../Components/Map.jsx"
 import './App.css'
 import { Suspense, useState } from "react"
 
-function Interaction() {
+function Interaction(props) {
   const { nodes } = useGLTF('../public/DASMA.glb')
-  const [ color, setColor] = useState('red')
-  
-  return (
-    <mesh 
-      geometry={nodes.Salawag.geometry} 
-      onClick={() => {
-        console.log("ClIcED SALAWAG")
-        setColor(color === 'red' ? 'blue' : 'red')  
-      }}  
-    >
-      <meshStandardMaterial color={color}/>      
-    </mesh>
+  const [ Salawag_Color, Salawag_setColor] = useState('gray')
+  const [ Salitran_IV_Color, Salitran_IV_setColor] = useState('gray')
 
+  const [ activeBarangay, setActiveBarangay ] = useState(null)
+
+  const handleClick = (name) => {
+    setActiveBarangay(prev => (prev === name? null : name))
+  }
+
+  const getColor = (name) => activeBarangay === name? 'red' : 'gray'
+
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        scale={19.644} 
+        position={[-1.468, 0.009, -7.783]}
+        geometry={nodes.Salawag.geometry} 
+        onClick={() => {
+          console.log("ClIcED SALAWAG")
+          handleClick('Salawag')
+        }}  
+      >
+        <meshStandardMaterial color={getColor('Salawag')}/>      
+      </mesh>
+      <mesh 
+        scale={19.644}
+        position={[-2.754, 0.009, -7.807]}
+        geometry={nodes.Salitran_IV.geometry} 
+        onClick={() => {
+          console.log("ClIcED SALITRAN")
+          handleClick('Salitran_IV')
+        }}  
+      >
+        <meshStandardMaterial color={getColor('Salitran_IV')}/>   
+      </mesh>  
+      
+    </group>  
   )
 }
 
@@ -28,10 +53,10 @@ export default function index() {
      <Canvas>
         <Suspense fallback={null}>
           <Environment preset="sunset" />
-
+          <PerspectiveCamera makeDefault position={[0, 20, 0]}></PerspectiveCamera>
           <OrbitControls autoRotate autoRotateSpeed={1}/> 
           
-          {/*<DasMap />*/}
+          <DasMap />
           <Interaction />
          </Suspense>
      </Canvas>
